cfaa5acc5fb63d485cdaea6e99961b2ba946aad7
cpb-home <cpb@bizb.ru>
2024-04-23 23:19:41 +0300

    task done

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -22,23 +22,21 @@ const Details = ({ info }: IDetails) => {
   const [currentUserInfo, setCurrentUserInfo] = useState<IUserInfo>();
 
   useEffect(() => {
-    let url: string;
-    if (!info || info.id === 0) {
-      url = '';
-    } else {
-      url = `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`;
-      fetch(url)
-        .then(res => res.json())
-        .then(result => setCurrentUserInfo(result));
+    if (info?.id !== currentUserInfo?.id) {
+      if (info && info.id !== 0) {
+        fetch(`https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`)
+          .then(res => res.json())
+          .then(result => setCurrentUserInfo(result));
+      }
     }
   }, [info]);
-  console.log(currentUserInfo?.avatar);
+  
 
   return (
     <div className="details">
       {currentUserInfo ? 
       <>
-        <div className="datailSubEl detailImgCont"><img src={currentUserInfo.avatar} alt={currentUserInfo.name} /></div> 
+        <div className="datailSubEl detailImgCont"><img src={currentUserInfo.avatar + '?img=' + currentUserInfo.id} alt={currentUserInfo.name} /></div> 
         <div className="datailSubEl datailHeader">{currentUserInfo.name}</div>
         <div className="datailSubEl">City: {currentUserInfo.details.city}</div>
         <div className="datailSubEl">Company: {currentUserInfo.details.company}</div>
@@ -54,17 +52,19 @@ export default Details
 
 /*
 
-return (
-    <div className="details">
-      {info.id !== 0 ? 
-        <div className="detailImgCont">
-          <img src="" alt="" />
-        </div>
-        <div className="datailHeader">
-          
-        </div> : 
-        "" }
-    </div>
-  )
+useEffect(() => {
+    if (info?.id !== currentUserInfo?.id) {
+      let url: string;
+      if (!info || info.id === 0) {
+        url = '';
+      } else {
+        url = `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${info.id}.json`;
+        fetch(url)
+          .then(res => res.json())
+          .then(result => setCurrentUserInfo(result));
+      }
+    }
+  }, [info]);
+  console.log(currentUserInfo?.avatar);
 
   */
